09038ebe91eeec80d9535cfe8bc8ae0ed5cd83ac
chathuranga0307 <chathuranga0307@gmail.com>
2019-07-04 14:54:59 +0530

    Add new C sharp practicals 1

---
+++ C Sharp Practicals/C Sharp Practicals/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace C_Sharp_Practicals
+{
+    class Class1
+    {
+        public int GetSum(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
---
+++ C Sharp Practicals/C Sharp Practicals/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace C_Sharp_Practicals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a,b,c;          
+           
+            Console.WriteLine("Enter Value 1");
+            a = int.Parse(Console.ReadLine());
+            Console.WriteLine(" Enter Value 2");
+            b =int.Parse( Console.ReadLine());
+
+
+
+
+            Class1 cs1 = new Class1();   //This is a creating an object class 1
+            c = cs1.GetSum(a,b);  //assigning  GetSum method on class 1 to c variable
+            Console.WriteLine("Summation: " + c); //calling GetSum method on class 1
+       
+            Console.ReadKey();
+
+
+
+
+        }
+    }
+}
